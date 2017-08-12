8198390fdfe099e08aa96e35f39a99c5f43164ca
zhangkangen <zhangkangen@gmail.com>
2017-08-13 03:47:26 +0800

    first Springboot app

---
+++ first-app/src/main/java/com/zh/BootApplication.java
@@ -0,0 +1,21 @@
+package com.zh;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class BootApplication {
+
+    @RequestMapping("/")
+    String home(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws  Exception{
+        SpringApplication.run(BootApplication.class,args);
+    }
+}
