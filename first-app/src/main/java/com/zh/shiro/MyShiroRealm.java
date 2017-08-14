af8e7ae01ebe3d808d61cab20d72a36422389c28
zhangxiaokang <zhangxiaokang@huimin100.cn>
2017-08-14 17:42:33 +0800

    shiro

---
+++ first-app/src/main/java/com/zh/shiro/MyShiroRealm.java
@@ -0,0 +1,49 @@
+package com.zh.shiro;
+
+import com.zh.domain.UserInfo;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.util.ByteSource;
+
+public class MyShiroRealm extends AuthorizingRealm {
+
+    /**
+     * 权限校验
+     *
+     * @param principalCollection
+     * @return
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        return null;
+    }
+
+    /**
+     * 登录校验
+     *
+     * @param authenticationToken
+     * @return
+     * @throws AuthenticationException
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+
+        String username = (String) authenticationToken.getPrincipal();
+        UserInfo userInfo = null;
+        if (userInfo == null) {
+            return null;
+        }
+        SimpleAuthenticationInfo authenticationInfo = new SimpleAuthenticationInfo(
+                userInfo,
+                userInfo.getPassword(),
+                ByteSource.Util.bytes("sss"),
+                getName()
+        );
+        return authenticationInfo;
+    }
+}
